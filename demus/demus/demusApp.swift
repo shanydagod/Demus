db391c83655bf7d6fade906da069ba72cc132441
shanydagod <shantia08.azizian@gmail.com>
2025-11-11 10:24:58 +0100

    initial commit

---
+++ demus/demus/demusApp.swift
@@ -0,0 +1,17 @@
+//
+//  demusApp.swift
+//  demus
+//
+//  Created by shantia azizian on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct demusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
